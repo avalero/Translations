b933987d76030e91eb39908c5f53b5842c1b614b
Alberto Valero <alberto.valero@bq.com>
2019-05-20 17:37:37 +0200

    updated with mutations

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,80 +1,176 @@
-import {GraphQLServer} from 'graphql-yoga';
+import { GraphQLServer } from 'graphql-yoga';
 
 interface ITranslation {
   literal: string;
   as_in?: string;
-  translation: string;
+  tr: string;
   translator?: string;
 }
 
-const es_translations: ITranslation[] = [
+interface ITranslationsObject {
+  en: ITranslation[];
+  es: ITranslation[];
+}
+
+const literals: string[] = ['hello', 'bye'];
+
+const esTranslations: ITranslation[] = [
   {
-    literal: "hello",
-    as_in: "Hola como estas",
-    translation: "hola",
-    translator: "Alberto Valero"
+    literal: 'hello',
+    as_in: 'Hola como estas',
+    tr: 'hola',
+    translator: 'Alberto Valero',
   },
   {
-    literal: "bye",
-    as_in: "Me voy, adios",
-    translation: "adios",
-    translator: "Luis Diaz"
-  }
+    literal: 'bye',
+    as_in: 'Me voy, adios',
+    tr: 'adios',
+    translator: 'Luis Diaz',
+  },
 ];
 
-const en_translations: ITranslation[] = [
+const enTranslations: ITranslation[] = [
   {
-    literal: "hello",
-    as_in: "Hello, how are you",
-    translation: "hello",
-    translator: "Alberto Valero"
+    literal: 'hello',
+    as_in: 'Hello, how are you',
+    tr: 'hello',
+    translator: 'Alberto Valero',
   },
   {
-    literal: "bye",
-    as_in: "Bye, see you",
-    translation: "bye",
-    translator: "Luis Diaz"
-  }
+    literal: 'bye',
+    as_in: 'Bye, see you',
+    tr: 'bye',
+    translator: 'Luis Diaz',
+  },
 ];
 
-
-const translations = {
-  es: es_translations,
-  en: en_translations,
+const translationsObject: ITranslationsObject = {
+  es: esTranslations,
+  en: enTranslations,
 };
-
 
 const typeDefs: string = `
   type Query {
-    translations(lang: String): [Translation!]!
-    translation(lang: String, literal: String): Translation
+    getTranslations(lang: String): [Translation!]!
+    getTranslation(lang: String, literal: String!): Translation
+    getLiterals: [String!]!
+  }
+
+  type Mutation {
+    createLiteral(literal:String!): String!
+    addTranslation(literal:String!, lang:String!, tr:String!, as_in:String): Translation!
   }
 
   type Translation{
     literal: String!
     as_in: String
-    translation: String!
-    author: String!
+    tr: String!
+    translator: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    translations(parent: any, args:{lang?:string}, ctx: any, info: any):Array<ITranslation>{
-      if(!args.lang){
+    getTranslations(
+      parent: any,
+      args: { lang?: string },
+      ctx: any,
+      info: any
+    ): ITranslation[] {
+      if (!args.lang) {
         throw new Error('No lang specified');
       }
-      if(args.lang === "es") return es_translations;
-      if(args.lang === "en") return en_translations;
-    }
-  }
-}
+
+      if (translationsObject[args.lang]) {
+        return translationsObject[args.lang];
+      }
+
+      throw new Error(`Unknown language ${args.lang}`);
+    },
+
+    getTranslation(
+      parent: any,
+      args: { lang?: string; literal: string },
+      ctx: any,
+      info: any
+    ): ITranslation {
+      if (!args.literal) {
+        throw new Error('No literal specified');
+      }
+
+      // default language english
+      const lang: string = args.lang || 'en';
+      if (translationsObject[lang]) {
+        return translationsObject[lang].find(tr => tr.literal === args.literal);
+      }
+    },
+
+    getLiterals(parent: any, args: any, ctx: any, info: any) {
+      return literals;
+    },
+  },
+  Mutation: {
+    createLiteral(parent, args: { literal: string }, ctx, info): string {
+      if (!args.literal) {
+        throw new Error('Literal required');
+      }
+
+      if (literals.some(lit => lit === args.literal)) {
+        throw new Error('Literal already in DB');
+      }
+
+      literals.push(args.literal);
+      return args.literal;
+    },
+
+    addTranslation(
+      parent: any,
+      args: { literal: string; lang: string; tr: string; as_in?: string },
+      ctx: any,
+      info: any
+    ): ITranslation {
+      if (!args.literal || !args.lang || !args.tr) {
+        throw new Error('Missing argument');
+      }
+
+      if (!translationsObject[args.lang]) {
+        throw new Error('Non existing language');
+      }
+
+      const existingLiteral = literals.some(lit => lit === args.literal);
+      if (!existingLiteral) {
+        throw new Error('Literal not found');
+      }
+
+      const translation: ITranslation = translationsObject[args.lang].find(
+        tr => tr.literal === args.literal
+      );
+
+      if (!translation) {
+        const newTr: ITranslation = {
+          tr: args.tr,
+          literal: args.literal,
+          translator: 'Alberto Valero',
+          as_in: args.as_in || '',
+        };
+        translationsObject[args.lang].push(newTr);
+        return newTr;
+      }
+
+      translation.tr = args.tr;
+      translation.as_in = args.as_in || '';
+      translation.translator = 'Alberto Valero';
+
+      return translation;
+    },
+  },
+};
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
 });
 
-server.start( () => {
-  console.log("The server is up");
+server.start(() => {
+  console.log('The server is up');
 });
